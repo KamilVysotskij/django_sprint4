27113a0a255fc58f99a805fe95f02fdb1bc851bb
Kamil Vysotskij <kamil.vysotskij@yandex.ru>
2024-06-09 18:45:51 +0500

    test 25/25 htd request path

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -156,7 +156,7 @@ class PostCreateView(LoginRequiredMixin, CreateView):
     def get_success_url(self):
         return reverse('blog:profile',
                        kwargs={'username': self.request.user.username})
-    
+
     # def get_context_data(self, **kwargs):
     #     context = super().get_context_data(**kwargs)
     #     context['edit_post_mode'] = False
